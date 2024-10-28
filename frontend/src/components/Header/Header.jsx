c47bc48d6ce0c1249b67b33dc947aca9acee32ef
FisterMedister <stefanjespersen8@gmail.com>
2024-10-28 12:49:48 +0100

    The website should now be responsive and the nav-bar should also scroll down to their respective elements

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <div className='header'>
       <div className="header-contents">
-        <h2>Order your favorite pizza&apos;s here</h2>
+        <h2>Order your favorite pizzas here</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae atque repudiandae modi asperiores omnis? Doloremque voluptatem dolorum at, blanditiis odit beatae incidunt quibusdam atque perferendis nisi officiis minima omnis amet totam similique, eligendi provident tempora, soluta debitis illum inventore sit.</p>
       <button>View Menu</button>
       </div>
